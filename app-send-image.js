692c1d62eee352f3d05f7e2880bc59365a891342
Billy Bennett <40703526+billythemusical@users.noreply.github.com>
2023-11-02 18:00:12 -0400

    updated examples

--- app-send-image.js
+++ app-send-image.js
@@ -1,14 +1,10 @@
 require('dotenv').config()
 
-var cron = require('node-cron')
 var Pushover = require('node-pushover')
 var path = require('path');
 
-// Remember that file paths are relative to the location they are launched from.
-// Thus to find a file that is in the images folder, which is two levels above, or back, 
-// from the current folder, we need to use ".." twice, as below.
 const myFilePath = path.join(__dirname, './the-command-prompt.jpeg')
-console.log("My image path: ", myFilePath)
+console.log(`Image path: ${myFilePath}`)
 
 var push = new Pushover({
     token: process.env.APP_TOKEN,
@@ -17,7 +13,13 @@ var push = new Pushover({
 
 function notify() {
     console.log('running notify')
-    push.send(process.env.USERKEY, "Test Subject", "My message", myFilePath, handleErrors)
+    push.send(
+        process.env.USERKEY,
+        "Subject goes here",
+        "Message goes here",
+        myFilePath,
+        handleErrors
+    )
 }
 
 function handleErrors(error, response) {
@@ -27,4 +29,3 @@ function handleErrors(error, response) {
 }
 
 notify()
-process.exit() // exits the script
